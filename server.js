df71f8841bb175923ae5e3ed100af7a1c810405b
prabhjot singh <sprabhjot768@gmail.com>
2020-12-07 14:38:40 +0530

    connected mongodb locally

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB=require('./config/db');
+
+
+//Connect database
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API Running");
