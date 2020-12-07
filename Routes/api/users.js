aff3470827a3bc07655f715799b381cea0af4bb7
prabhjot singh <sprabhjot768@gmail.com>
2020-12-07 16:13:31 +0530

    added user registration functionality

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const Joi = require("joi");
+const gravatar = require("gravatar");
+const User = require("../../Models/User");
+const bcrypt = require("bcryptjs");
 
 //@route  POST api/users
 //@desc   Register user
 //@access public
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const schema = Joi.object({
     name: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().min(6).required(),
+    avatar: Joi.string(),
+    date: Joi.date(),
   });
 
   const { error } = schema.validate(req.body);
@@ -18,7 +25,46 @@ router.post("/", (req, res) => {
     return;
   }
 
-  res.send("User Route without error");
+  const { name, email, password } = req.body;
+
+  try {
+    //See if user exists
+
+    let user = await User.findOne({
+      email: email,
+    });
+
+    if (user) {
+      res.status(400).send("user already exist");
+    }
+    //get users gravatar
+
+    const avatar = gravatar.url(email, {
+      s: "200",
+      r: "pg",
+      d: "mm",
+    });
+
+    user = new User({
+      name,
+      email,
+      avatar,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
+    //encrypt password using bcrypt
+    await user.save();
+
+    res.send("User Registered");
+
+    //return jwt
+  } catch (err) {
+    res.status(500).status("Server error");
+  }
 });
 
 module.exports = router;
--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
+      useCreateIndex:true
     });
     console.log("MongoDB Connected");
   } catch (err) {
